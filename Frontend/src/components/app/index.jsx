23a77324acee299a2d47534a231f34aa67ff391f
svetlana <pivovarova.svetlana@gmail.com>
2022-12-06 17:45:29 +0400

    [ Feat ] add unstyled input

--- Frontend/src/components/app/index.jsx
+++ Frontend/src/components/app/index.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
 import { CustomButton } from './../../components/button/index';
+import { CustomInputBasic } from './../../components/input/index';
 
 function App() {
   return (
-    <CustomButton onClick={() => console.log('click!')}>LOG IN</CustomButton>
+      <div>
+        <CustomButton onClick={() => console.log('click!')}>LOG IN</CustomButton>
+        <CustomInputBasic />
+      </div>
+
   );
 }
 
