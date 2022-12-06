13a5a8335ad4a4813bf374f26fde7f9836818eec
Irina <myr-irina2@yandex.ru>
2022-12-06 10:26:09 +0300

    [Fix] delete custom button disabled

--- Frontend/src/components/app/index.jsx
+++ Frontend/src/components/app/index.jsx
@@ -1,15 +1,9 @@
 import React from 'react';
-import Stack from '@mui/material/Stack';
 import { CustomButton } from './../../components/button/index';
 
 function App() {
   return (
-    <Stack spacing={2} direction="row">
-      <CustomButton onClick={() => console.log('click!')}>LOG IN</CustomButton>
-      <CustomButton disabled focusableWhenDisabled>
-        LOG IN
-      </CustomButton>
-    </Stack>
+    <CustomButton onClick={() => console.log('click!')}>LOG IN</CustomButton>
   );
 }
 
