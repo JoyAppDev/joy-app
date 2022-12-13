eb38d5c8a264f443b953b7ac707871a5632d9245
svetlana <pivovarova.svetlana@gmail.com>
2022-12-13 10:49:58 +0400

    [ Feat ] add auth form in additional route

--- Frontend/src/components/app/index.jsx
+++ Frontend/src/components/app/index.jsx
@@ -1,12 +1,19 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Login from '../../pages/login';
+import {AuthForm} from "../auth-form";
 
 function App() {
   return (
-    <Routes>
-      <Route path="login" element={<Login />} />
-    </Routes>
+      <>
+          <Routes>
+              <Route path="login" element={<Login />} />
+          </Routes>
+          <Routes>
+              <Route path="auth" element={<AuthForm />} />
+          </Routes>
+      </>
+
   );
 }
 
