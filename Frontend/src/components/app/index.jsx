16c55c8275beb0c26e1936fc6dad38b66e4a0940
Irina <myr-irina2@yandex.ru>
2022-12-14 17:12:07 +0300

    [Fix] fix eslint comments

--- Frontend/src/components/app/index.jsx
+++ Frontend/src/components/app/index.jsx
@@ -2,15 +2,16 @@ import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Login from '../../pages/login';
 import HorizontalStepper from '../stepper';
-import {AuthForm} from "../auth-form";
+import { AuthForm } from '../auth-form';
 
 function App() {
   return (
-      <Routes>
-          <Route exact path="/" element={<Login />} />
-          <Route path="stepper" element={<HorizontalStepper />} />
-          <Route path="auth" element={<AuthForm />} /> // form with typescript, subsidiary
-      </Routes>
+    <Routes>
+      <Route exact path="/" element={<Login />} />
+      <Route path="stepper" element={<HorizontalStepper />} />
+      <Route path="auth" element={<AuthForm />} />
+      {/* form with typescript, subsidiary */}
+    </Routes>
   );
 }
 
