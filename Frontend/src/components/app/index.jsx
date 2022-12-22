b4b8b54fd64b9f37f0ce88b56d90df70a413be22
svetlana <pivovarova.svetlana@gmail.com>
2022-12-22 09:47:43 +0400

    [ Fix ] delete unused routes and comments

--- Frontend/src/components/app/index.jsx
+++ Frontend/src/components/app/index.jsx
@@ -1,18 +1,13 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Login from '../../pages/login';
-import HorizontalStepper from '../stepper';
 import SignIn from '../../pages/signin';
-import RegistrationFinalStep from '../registration-step-final';
 
 function App() {
   return (
     <Routes>
       <Route exact path="/" element={<Login />} />
-      <Route path="stepper" element={<HorizontalStepper />} />{' '}
-      {/* subsidiary */}
       <Route path="register" element={<SignIn />} />
-        <Route path="final" element={<RegistrationFinalStep />} />
     </Routes>
   );
 }
