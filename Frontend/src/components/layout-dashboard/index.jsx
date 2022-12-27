65cbfd6cd752e6d564a4f0b7466ddd4262defc53
svetlana <pivovarova.svetlana@gmail.com>
2022-12-27 11:10:53 +0400

    [ Feat ] add HeaderDashboard to LayoutDashboard

--- Frontend/src/components/layout-dashboard/index.jsx
+++ Frontend/src/components/layout-dashboard/index.jsx
@@ -3,15 +3,17 @@ import React from "react";
 import Box from '@mui/material/Box';
 
 import starDashboard from './../../assets/star_dashboard_x1.png';
-import RecipeReviewCard from './card';
+import HeaderDashboard from '../header-dashboard';
 
 
 function LayoutDashboard() {
     return (
         <>
+            <HeaderDashboard />
             <Box sx={{
                 width: '100%',
                 height: '100vh',
+                mt: 9,
                 backgroundImage: `url(${starDashboard})`,
                 backgroundPosition: 'bottom right',
                 backgroundRepeat: 'no-repeat',
@@ -20,13 +22,7 @@ function LayoutDashboard() {
                 boxSizing: 'border-box',
             }}>
                 <Box display="grid" gridTemplateColumns="repeat(4, 1fr)" gap={5}>
-                    <RecipeReviewCard sx={{}} />
-                    <RecipeReviewCard />
-                    <RecipeReviewCard />
-                    <RecipeReviewCard />
-                    <RecipeReviewCard />
-                    <RecipeReviewCard />
-
+                    {/* здесь будут карточки */}
                 </Box>
             </Box>
         </>
