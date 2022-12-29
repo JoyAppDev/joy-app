aec4b68f20ac6ceb3514eaba9a64ce193f5f9fac
Irina <myr-irina2@yandex.ru>
2022-12-29 13:31:51 +0300

    [Feat] add modal

--- Frontend/src/components/app/index.jsx
+++ Frontend/src/components/app/index.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Login from '../../pages/login';
 import SignIn from '../../pages/signin';
+import BasicModal from '../modal';
 
 function App() {
   return (
     <Routes>
       <Route exact path="/" element={<Login />} />
       <Route path="register" element={<SignIn />} />
+      <Route path="modal" element={<BasicModal />} />
     </Routes>
   );
 }
