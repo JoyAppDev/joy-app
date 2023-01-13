718ae50c6b99585c5a76b39f37139ea59e17d448
svetlana <pivovarova.svetlana@gmail.com>
2023-01-13 15:56:58 +0400

    [ Feat ] delete file upload concept check

--- Frontend/src/components/app/index.jsx
+++ Frontend/src/components/app/index.jsx
@@ -3,7 +3,6 @@ import { Routes, Route } from 'react-router-dom';
 import Login from '../../pages/login';
 import SignIn from '../../pages/signin';
 import Dashboard from '../../pages/dashboard';
-import Upload from "../upload";
 
 function App() {
   return (
@@ -12,7 +11,6 @@ function App() {
         <Route exact path="/" element={<Login />} />
         <Route path="register" element={<SignIn />} />
         <Route path="dashboard" element={<Dashboard />} />
-        <Route path="upload" element={<Upload />} />
       </Routes>
     </div>
   );
