0e2f354e42ff572e0e8aae053b5ccc525f01a9ac
svetlana <pivovarova.svetlana@gmail.com>
2023-01-13 10:23:39 +0400

    [ Fix ] delete cursor pointer and onClick from the image of cards

---
+++ Frontend/src/components/licence-add-card/index.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import LicenceCard from "../../components/licence-card";
+import addLicenceImage from "../../assets/add_licence_image.png";
+
+function LicenceAddCard() {
+    return (
+        <LicenceCard
+            image={addLicenceImage}
+        />
+    )
+}
