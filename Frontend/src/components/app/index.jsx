805f7e371f181832da070dccff9166222513f75d
svetlana <pivovarova.svetlana@gmail.com>
2023-01-31 11:50:39 +0400

    [ REFACTOR ] add constance for pathname

--- Frontend/src/components/app/index.jsx
+++ Frontend/src/components/app/index.jsx
@@ -1,22 +1,19 @@
 import React from 'react';
-import {Routes, Route, useNavigate} from 'react-router-dom';
+import { Routes, Route, useNavigate } from 'react-router-dom';
 import Login from '../../pages/login';
 import SignIn from '../../pages/signin';
 import Dashboard from '../../pages/dashboard';
 import * as auth from '../../utils/auth';
-import { CurrentUserContext } from "../../contexts/CurrentUserContext";
-import RequireAuth from "../../hoc/RequireAuth";
-//import ProtectedRoute from "../protected-route";
+import { CurrentUserContext } from '../../contexts/CurrentUserContext';
+import RequireAuth from '../../hoc/RequireAuth';
+import { CREATOR_DASHBOARD, LOGIN } from '../../utils/constants';
 
 function App() {
     const [currentUser, setCurrentUser] = React.useState({});
-    const [loggedIn, setLoggedIn] = React.useState(false);
 
     let navigate = useNavigate();
 
     // check token for authorization
-    // const token = '0b7d621f3284500d0f6bb091758199b94cb6d99a';
-
     React.useEffect(() => {
         checkToken()
     }, []);
@@ -29,17 +26,14 @@ function App() {
             auth.checkToken(token)
                 .then((response) => {
                     setCurrentUser(response.username);
-                    setLoggedIn(true);
-                    navigate('/dashboard');
-                    console.log("user", response.username);
+                    navigate(CREATOR_DASHBOARD);
                 })
                 .catch((err) => {
                     console.log(err);
                 })
         }
         else if (token === null) {
-            //handleSignOut();
-            alert('no token');
+            navigate(LOGIN);
         }
     }
 
--- Frontend/src/utils/constants.js
+++ Frontend/src/utils/constants.js
@@ -1,3 +1,6 @@
 export const REQUIRED_FIELD = 'Required field';
 export const PASSWORD_LENGTH_ERROR = 'Password must be longer than 3 characters';
 export const API_URL = 'http://127.0.0.1:8000';
+
+export const CREATOR_DASHBOARD = '/dashboard';
+export const LOGIN = '/';
