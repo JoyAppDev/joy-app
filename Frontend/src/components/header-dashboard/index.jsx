cadb0fa48884f3368fd4b9448c2fca97db2d7708
Irina <myr-irina2@yandex.ru>
2023-02-13 16:53:34 +0300

    [Refactor] fix dashboard layout

--- Frontend/src/components/header-dashboard/index.jsx
+++ Frontend/src/components/header-dashboard/index.jsx
@@ -11,28 +11,40 @@ function HeaderDashboard() {
     <div className="header__dashboard">
       <Box
         sx={{
-          height: 75,
-          width: '100%',
-          pl: 15,
-          pr: 15,
-          display: 'flex',
-          justifyContent: 'space-between',
-          alignItems: 'center',
           boxShadow:
             '0px 2px 1px -1px rgba(0, 0, 0, 0.2), 0px 1px 1px rgba(0, 0, 0, 0.14), 0px 1px 3px rgba(0, 0, 0, 0.12)',
-          boxSizing: 'border-box',
         }}
       >
-        <Stack direction="row" spacing={5}>
-          <img
-            src={logo}
-            alt="{item.title}"
-            loading="lazy"
-            style={{ width: 180 }}
-          />
-          <HeaderTabs sx={{ alignSelf: 'end' }} />
-        </Stack>
-        <AccountMenu />
+        <Box
+          maxWidth="lg"
+          sx={{
+            height: 75,
+            width: '100%',
+            display: 'flex',
+            justifyContent: 'space-between',
+            alignItems: 'center',
+            boxSizing: 'border-box',
+            margin: '0 auto',
+            borderBottom: 1,
+            borderColor: 'divider',
+          }}
+        >
+          <Stack
+            direction="row"
+            spacing={5}
+            alignItems="center"
+            justifyContent="center"
+          >
+            <img
+              src={logo}
+              alt="{item.title}"
+              loading="lazy"
+              style={{ width: 103, height: 35 }}
+            />
+            <HeaderTabs sx={{ alignSelf: 'end' }} />
+          </Stack>
+          <AccountMenu />
+        </Box>
       </Box>
     </div>
   );
